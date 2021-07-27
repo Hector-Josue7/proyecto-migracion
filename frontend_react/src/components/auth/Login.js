6051dd3db68b68f67104bc1739bbc02893e552a3
Hector-Josue7 <hectorardonm@gmail.com>
2021-07-27 09:31:07 -0600

    cambios antes de realizar modificaciones al dashboard

--- frontend_react/src/components/auth/Login.js
+++ frontend_react/src/components/auth/Login.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useState,  useContext } from "react";  //
 import { useHistory} from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
-import logoDinaf from '../../img/logodinaf-01.png'
+import logoDinaf from '../../assets/img/logodinaf-01.png'
 import './Login.css'
 
 import Swal from 'sweetalert2';
