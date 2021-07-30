b972cfc29753172a720b04f6df23449f992c8c96
Hector-Josue7 <hectorardonm@gmail.com>
2021-07-29 21:42:49 -0600

    cambios desde la casa Jueves 29/07/2021

--- frontend_react/src/pages/Tablero.js
+++ frontend_react/src/pages/Tablero.js
@@ -1,5 +1,6 @@
 
 import React, {useState} from "react"; // useEffect
+import axios from "axios";
 
 //  import Navbar from "../components/Navbar";
 // import {Container, Row, Col, Card, Form, Button } from "react-bootstrap";
@@ -9,9 +10,9 @@ import React, {useState} from "react"; // useEffect
 
  function Tablero() {
 
-  const [expediente, setBuscarExpediente] = useState('');
-
  
+
+  const [expediente, setBuscarExpediente] = useState('');
   //  async function devuelveExpediente() {
 
   //   const resp = await axios.get(`http://localhost:3006/nice/expedientes/${expediente}`);
@@ -25,11 +26,27 @@ import React, {useState} from "react"; // useEffect
   // }, [] );
 // React Hook useEffect has a missing dependency 
 
-return (
+
+async function getExpediente(e){
+  e.preventDefault();
+  try{
+    const response = await axios.get(`http://localhost:3007/nice/expedientes/${expediente}`);
+
+ document.write(response.data[0])
+   
+  }catch(err){
+    console.error(err);
+  }
+}
+
+
+
+
+  return (
     <div>
       {/* <Navbar  /> */}
     
-      <form className="d-flex" style ={{marginTop: '-220px'}}  >
+      <form onSubmit={getExpediente} className="d-flex" style ={{marginTop: '-220px'}}  >
       <p>Ingrese el registro del expediente</p><br/>
         <input className="form-control me-2" style ={{ width: '400px'}} type="search" placeholder="Search" aria-label="Search"  
          onChange={(e) => setBuscarExpediente(e.target.value)}   value={expediente}/>
@@ -40,6 +57,8 @@ return (
       
     </div>
   );
+
+
 }
 
 export default Tablero;
